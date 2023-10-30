be319cb3dea266b6a5001e686d272a50da2e32ec
Jaeeun, Lee <hijaeeun@amazon.com>
2023-10-31 00:02:11 +0900

    initialize commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by Lee, Jaeeun on 2023/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var sum = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+    }
+    
+    override func viewSafeAreaInsetsDidChange() {
+        
+    }
+}
+
